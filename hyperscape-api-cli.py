c427cedae37ebce6e4f34b6c580ebbe75e3e68eb
Sh211 <sh211@users.noreply.github.com>
2020-08-14 22:04:50 +0100

    Removed the timestamp field of the url as it is autofilled by the API and caused errors when manually set.

--- hyperscape-api-cli.py
+++ hyperscape-api-cli.py
@@ -27,19 +27,22 @@ request = urllib.request.Request(url.format(platform, user), data=None, headers=
 with urllib.request.urlopen(request) as openURL:
     data = json.loads(openURL.read())
 uid = str(data["players"].keys())[12:48]
-uidURL = "https://hypers.apitab.com/player/{}?x={}"
-request = urllib.request.Request(uidURL.format(uid, 1597420964), data=None, headers={"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"})
+uidURL = "https://hypers.apitab.com/player/{}"
+request = urllib.request.Request(uidURL.format(uid), data=None, headers={"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"})
 with urllib.request.urlopen(request) as openURL:
     fullData = json.loads(openURL.read())
-print("Victories : " + str(fullData["data"]["stats"]["wins"]) +
-"\nCrown Victories : " + str(fullData["data"]["stats"]["crown_wins"]) +
-"\nOverall Damage : " + str(fullData["data"]["stats"]["damage"]) +
-"\nMatches : " + str(fullData["data"]["stats"]["matches"]) +
-"\nKills : " + str(fullData["data"]["stats"]["kills"]) +
-"\nFusions : " + str(fullData["data"]["stats"]["fusions"]) +
-"\nTime Played : " + str(fullData["data"]["stats"]["time_played"]) +
-"\nCareer Best: Kills : " + str(fullData["data"]["stats"]["careerbest_kills"]) +
-"\nCareer Best: Damage Done : " + str(fullData["data"]["stats"]["careerbest_damage_done"]) +
-"\nKD : " + str(fullData["data"]["stats"]["kd"]))
+try:
+    print("Victories : " + str(fullData["data"]["stats"]["wins"]) +
+    "\nCrown Victories : " + str(fullData["data"]["stats"]["crown_wins"]) +
+    "\nOverall Damage : " + str(fullData["data"]["stats"]["damage"]) +
+    "\nMatches : " + str(fullData["data"]["stats"]["matches"]) +
+    "\nKills : " + str(fullData["data"]["stats"]["kills"]) +
+    "\nFusions : " + str(fullData["data"]["stats"]["fusions"]) +
+    "\nTime Played : " + str(fullData["data"]["stats"]["time_played"]) +
+    "\nCareer Best: Kills : " + str(fullData["data"]["stats"]["careerbest_kills"]) +
+    "\nCareer Best: Damage Done : " + str(fullData["data"]["stats"]["careerbest_damage_done"]) +
+    "\nKD : " + str(fullData["data"]["stats"]["kd"]))
+except AttributeError:
+    print("The statistics for that username could not be retrieved.")
 pause()
 
